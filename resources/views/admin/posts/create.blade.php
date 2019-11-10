821a63e9ca20e25388f8d991bb26167050712cab
Nayeem Kazi <psubangladesh@gmail.com>
2019-11-10 17:24:12 +0600

    Added Post Create Route

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,13 @@
+@extends('../../layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1>Create Me brother </h1>
+                Create
+            
+        </div>
+    </div>
+</div>
+@endsection
